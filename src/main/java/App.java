b0a4a94c1b1ffbb8976d1d5342edf0dbadf37b06
Yago Nunes da Silva <138827369+nunesyago02@users.noreply.github.com>
2025-04-02 14:54:51 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,24 +2,24 @@ package main.java;
 import java.util.Scanner;
 
 public class App {
-        //Comentário
-
     public static void main(String[] args)throws Exception{
+
         //Entrada dos dados
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a massa kg: ");
         double massa = scanner.nextDouble();
-
         System.out.print("Digite a altura m: ");
         double altura = scanner.nextDouble();
+        
+       //Fechamento do scanner após entrada 
+       scanner.close();
 
-        scanner.close();//Fechamento do scanner após entrada
         //Criação do objeto e calculo de imc
         CorpoHumano corpo = new CorpoHumano();
         corpo.setmassa(massa);
         corpo.setaltura(altura);
-
         double imc = corpo.Calcimc();
+
         //Saída de resultado 
         System.out.print("Seu imc é " +imc);
     }
